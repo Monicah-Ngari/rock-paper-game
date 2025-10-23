35a49a7736b9c798d0d5f42730198a6cbb17a14b
Monicah <wakarimangari@gmail.com>
2025-10-23 20:28:32 +0300

    added some functionality to my buttons

---
+++ index.js
@@ -0,0 +1,11 @@
+function clicksRock() {
+alert('You clicked Rock!');
+}
+function clicksPaper() {
+alert('You clicked Paper!');
+
+}
+function clicksScissors() {
+alert('You clicked Scissors!');
+
+}
