f57f5daa9aa07f8a7edbdc26aed692cc1f8b86d0
Monicah <wakarimangari@gmail.com>
2025-10-24 07:37:52 +0300

    added a new funtion for compouterMove

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+function computerMove() {
+  let computerChoice = Math.random();
+  if (computerChoice <= 0 && computerChoice <1/3){
+    alert('The computer chose Rock!');
+  }
+  else if (
+      computerChoice >= 1/3 && computerChoice <2/3){
+        alert('The computer chose Paper!');
+  }
+  else (computerChoice <=2/3 && <1){
+          alert('The computer chose Scissors!');
+        }
+}
+  
+
+
 function clicksRock() {
 alert('You clicked Rock!');
 }
+
 function clicksPaper() {
 alert('You clicked Paper!');
 
