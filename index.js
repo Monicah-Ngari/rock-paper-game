7a987f07e62ac0d40593efba17fd12d5172e1200
Monicah <wakarimangari@gmail.com>
2025-11-06 20:15:25 +0300

    added some js content

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ function computerMove() {
   alert(`The computer chose ${choice}!`);
   return choice; 
 }
-  
+
+let userScore = 0;
+let computerScore = 0;
+ function updateWInner () {
+  document .getELementById=(your-score).innerText = userScore;
+  document .getELementById=(computer-score).innerText = computerScore;
+
+
+  let winner = '';
+  if (userScore > computerScore) {
+    winner = 'You are the overall winner! 🎉';
+
+  }
+  else if (computerScore > userScore) {
+    winner = 'Computer is the overall winner! 🎉';
+  }
+  else {
+    winner = 'The game is a tie overall!';
+  }
+
+  document.getElementById('winner').innerText = winner;
+ }
 
 
 function clicksRock() {
@@ -22,9 +43,11 @@ function clicksRock() {
   if (computer === 'Rock') {
     alert('It\'s a tie!');
   } else if (computer === 'Paper') {
-    alert('You lose 😞! Computer wins!');}
+    alert('You lose 😞! Computer wins!');
+    computerScore++;}
   else if (computer === 'Scissors') {
     alert('You win 😊!');}
+  userScore++;
 
 
 }
@@ -33,22 +56,25 @@ function clicksPaper() {
   const computer = computerMove(); 
   if 
   (computer === 'Rock') {
-    alert('You win 😊!');}
+    alert('You win 😊!');
+  computerScore++;}
+    
   else if (computer === 'Paper') {
     alert('It\'s a tie!');}
   else if (computer === 'Scissors') {
-    alert('You lose 😞! Computer wins!');}
+    alert('You lose 😞! Computer wins!');
+    userScore++;}
 }
 function clicksScissors() {
   const computer = computerMove(); 
   if (computer === 'Rock') {
-    alert('You lose 😞! Computer wins!');}
+    alert('You lose 😞! Computer wins!');
+    computerScore++;}
   else if (computer === 'Paper') {
-    alert('You win 😊!');}
+    alert('You win 😊!');
+    userScore++;}
   else if (computer === 'Scissors') {
     alert('It\'s a tie!');} 
 
 }
 
-let userScore = 0;
-let computerScore = 0;
